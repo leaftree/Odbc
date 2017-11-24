62ed618a3adfdb90531738fc7f1c0d180543d8c0
afc41 <afc41@cssweb.com.cn>
2017-11-24 17:29:41 +0800

    update

--- src/List.c
+++ src/List.c
@@ -1,85 +1,198 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
-/**
- * å­—æ®µåŸŸä¿¡æ¯
- */
-typedef struct FILDE_ATTR
-{
-	int              nFieldSize;
-	int              nFieldType;
-	char             szFieldName[128];
-	struct list_head List;
-} FIELD_ATTR;
-
-/**
- * è¡¨ç»“æ„ä¿¡æ¯
- */
-typedef struct TABLE_INFO
-{
-	int         nTotalSize;
-	int         nFieldCounter;
-	char        szTableName[128];
-	FIELD_ATTR *pFieldAttrList;
-} TABLE_STRUCTURE;
+#include <mcheck.h>
 
 typedef struct list_head list_head;
 
+typedef struct FIELD_ATTR FIELD_ATTR;
+typedef struct TABLE_STRUCTURE TABLE_STRUCTURE;
+
+/**
+ * ×Ö¶ÎÓòĞÅÏ¢
+ */
+struct FIELD_ATTR
+{
+    int              nFieldSize;
+    int              nFieldType;
+    char             szFieldName[128];
+    struct list_head List;
+};
+
+/**
+ * ±í½á¹¹ĞÅÏ¢
+ */
+struct TABLE_STRUCTURE
+{
+    int         nTotalSize;
+    int         nFieldCounter;
+    char        szTableName[128];
+    list_head  *pCursor;
+    list_head  *pListRoot;
+
+    void (*FreeFieldAttrList)     (TABLE_STRUCTURE *);
+    int  (*AddFieldAttrNodeTail)  (TABLE_STRUCTURE *, FIELD_ATTR *);
+    int  (*FetchNextFieldAttrNode)(TABLE_STRUCTURE *, FIELD_ATTR **);
+};
+
+int FetchNextFieldAttrNode(TABLE_STRUCTURE *pTabStruct, FIELD_ATTR **ppField);
+void FreeFieldAttrList(TABLE_STRUCTURE *pTabStruct);
+
 void *NewFieldAttrNode()
 {
-	FIELD_ATTR *new = malloc(sizeof(FIELD_ATTR));
-	if(!new)
-		return NULL;
+    FIELD_ATTR *new = malloc(sizeof(FIELD_ATTR));
+    if(!new)
+        return NULL;
 
-	new->nFieldSize = 0;
-	new->nFieldType = 0;
-	*new->szFieldName= 0;
-	INIT_LIST_HEAD(&new->List);
+    new->nFieldSize = 0;
+    new->nFieldType = 0;
+    *new->szFieldName= 0;
+    INIT_LIST_HEAD(&new->List);
 
-	return new;
+    return new;
+}
+
+void FreeFieldAttrList(TABLE_STRUCTURE *pTabStruct)
+{
+    list_head *n, *pos, *root;
+
+    if(!pTabStruct)
+        return;
+
+    FIELD_ATTR *entry;
+
+    root = pTabStruct->pListRoot;
+
+    list_for_each_safe(pos, n, root)
+    {
+        list_del(pos);
+        entry = list_entry(pos, FIELD_ATTR, List);
+        free(entry);
+    }
+
+    free(pTabStruct->pListRoot);
+    pTabStruct->pListRoot = NULL;
+    pTabStruct->pCursor = NULL;
+}
+
+int AddFieldAttrNode(TABLE_STRUCTURE *pTabStruct, FIELD_ATTR *new)
+{
+    if(!pTabStruct || !new || !pTabStruct->pListRoot)
+        return -1;
+
+    list_add(&new->List, pTabStruct->pListRoot);
+
+    return 0;
+}
+
+int AddFieldAttrNodeTail(TABLE_STRUCTURE *pTabStruct, FIELD_ATTR *pNew)
+{
+    if(!pTabStruct || !pTabStruct->pListRoot || !pNew)
+        return -1;
+
+    list_add_tail(&pNew->List, pTabStruct->pListRoot);
+
+    return 0;
+}
+
+int FetchNextFieldAttrNode(TABLE_STRUCTURE *pTabStruct, FIELD_ATTR **ppField)
+{
+    FIELD_ATTR *field;
+
+    if(!pTabStruct || !pTabStruct->pCursor || !ppField)
+        return -1;
+
+    if(list_empty(pTabStruct->pListRoot) || pTabStruct->pCursor->next==pTabStruct->pListRoot)
+    {
+        pTabStruct->pCursor = pTabStruct->pListRoot;
+        return 1;
+    }
+
+    field = list_entry(pTabStruct->pCursor->next, FIELD_ATTR, List);
+    pTabStruct->pCursor = pTabStruct->pCursor->next;
+
+    *ppField=field;
+
+    return 0;
+}
+
+/*
+int FetchAllFieldAttrNode(TABLE_STRUCTURE *pTabStruct, FIELD_ATTR *ppField)
+{
+    if(!pTabStruct || !pTabStruct->pCursor || !ppField)
+        return -1;
+
+    static list_head *pCursor;
+
+   list_for_each_safe(pos, n, TabStruct.pListRoot)
+   {
+   field = list_entry(pos, FIELD_ATTR, List);
+return 0;
+}
+*/
+
+TABLE_STRUCTURE *NewTableStruct()
+{
+    TABLE_STRUCTURE *pTabStruct = malloc(sizeof(TABLE_STRUCTURE));
+
+    list_head *root = malloc(sizeof(list_head));
+    INIT_LIST_HEAD(root);
+
+    pTabStruct->pCursor = root;
+    pTabStruct->pListRoot = root;
+    pTabStruct->FreeFieldAttrList = FreeFieldAttrList;
+    pTabStruct->AddFieldAttrNodeTail = AddFieldAttrNodeTail;
+    pTabStruct->FetchNextFieldAttrNode = FetchNextFieldAttrNode;
+
+    return pTabStruct;
 }
 
 int main()
 {
-	list_head *root = malloc(sizeof(list_head));
-	INIT_LIST_HEAD(root);
+    TABLE_STRUCTURE TabStruct;
 
-	FIELD_ATTR *new = NULL;
+    FIELD_ATTR *new = NULL;
 
-	new = NewFieldAttrNode();
-	list_add_tail(&new->List, root);
-	new->nFieldSize = 10;
-	new->nFieldType = 2;
-	sprintf(new->szFieldName, "%s", "ShenSiYuan");
+    new = NewFieldAttrNode();
+    new->nFieldSize = 10;
+    new->nFieldType = 2;
+    sprintf(new->szFieldName, "%s", "ShenSiYuan");
+    TabStruct.AddFieldAttrNodeTail(&TabStruct, new);
 
-	new = NewFieldAttrNode();
-	list_add_tail(&new->List, root);
-	new->nFieldSize = 110;
-	new->nFieldType = 2;
-	sprintf(new->szFieldName, "%s", "I love");
+    new = NewFieldAttrNode();
+    new->nFieldSize = 110;
+    new->nFieldType = 2;
+    sprintf(new->szFieldName, "%s", "LiuYF");
+    TabStruct.AddFieldAttrNodeTail(&TabStruct, new);
 
-	struct list_head *pos;
-	FIELD_ATTR *field;
+    new = NewFieldAttrNode();
+    new->nFieldSize = 1110;
+    new->nFieldType = 3;
+    sprintf(new->szFieldName, "%s", "And Love ");
+    TabStruct.AddFieldAttrNodeTail(&TabStruct, new);
 
-	list_for_each(pos, root)
-	{
-		field = list_entry(pos, FIELD_ATTR, List);
-		printf("%d %d %s\n", field->nFieldSize, field->nFieldType, field->szFieldName);
+    int iRet=0;
+    new=NULL;
+    while(iRet==0)
+    {
+        iRet = TabStruct.FetchNextFieldAttrNode(&TabStruct, &new);
+        if(iRet!=0)
+            break;
+        printf("%d %d %s\n", new->nFieldSize, new->nFieldType, new->szFieldName);
+    }
 
-		list_del(pos);
-		free(field);
-	}
+    printf("abc\n");
 
-	printf("ssy\n");
+    iRet=0;
+    while(iRet==0)
+    {
+        iRet = TabStruct.FetchNextFieldAttrNode(&TabStruct, &new);
+        if(iRet!=0)
+            break;
+        printf("%d %d %s\n", new->nFieldSize, new->nFieldType, new->szFieldName);
+    }
 
-	list_for_each(pos, root)
-	{
-		field = list_entry(pos, FIELD_ATTR, List);
-		printf("%d %d %s\n", field->nFieldSize, field->nFieldType, field->szFieldName);
-	}
+    TabStruct.FreeFieldAttrList(&TabStruct);
 
-	list_del(root);
-
-	return 0;
+    return 0;
 }
